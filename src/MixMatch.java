280e27e1ebdb3e8ff838716b83733e9f0fefb6c5
Cedric <124837095+cswxyz@users.noreply.github.com>
2025-02-03 04:00:55 +0000

    done tbh

---
+++ src/MixMatch.java
@@ -0,0 +1,22 @@
+public class MixMatch {
+    public static void main (String arg[]){
+    String name = "Cedric";
+    char lastInitial = "S";
+    short number = 3200;
+    int integer = 7;
+    long longNumber = 1234123;
+    float decimal = 32.0;
+    double doub = 32.67;
+    boolean bool = true;
+
+    System.out.println(name + " prints text characters");
+    System.out.println(lastInitial + " prints single string characters");
+    System.out.println(number + " a shorter range of integers");
+    System.out.println(integer + "represents integeres from -127 to 128");
+    System.out.println(longNumber + " also an integer value with longer range");
+    System.out.println(decimal + "can represent numbers as decimals with 7 levels of accuracy");
+    System.out.println(doub + " can represent numbers as deciamsl with more accuracy and range than decimal");
+    System.out.println(bool+ "represents true or false values");
+
+    }
+}
